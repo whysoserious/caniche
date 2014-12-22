354845a16b7ee1649e53d1ca0f5b11fc27fe2d15
Jan Ziniewicz <janek.ziniewicz@gmai.com>
2014-12-22 22:29:19 +1100

    New deps

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [
-                 [org.clojure/clojure "1.6.0"]
-                 [clj-http "1.0.1"]
+  :dependencies [[clj-http "1.0.1"]
+                 [compojure "1.3.1"]
                  [crypto-random "1.2.0"]])
+                 [org.clojure/clojure "1.6.0"]
+                 [ring/ring-core "1.3.2"]
+                 [ring/ring-jetty-adapter "1.3.2"]])
+

bf5e9241fb5366b8050dc7a6ff691d8e93884294
Jan Ziniewicz <janek.ziniewicz@gmai.com>
2014-12-22 22:32:56 +1100

    Fix compilation errors

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[clj-http "1.0.1"]
                  [compojure "1.3.1"]
-                 [crypto-random "1.2.0"]])
+                 [crypto-random "1.2.0"]
                  [org.clojure/clojure "1.6.0"]
                  [ring/ring-core "1.3.2"]
                  [ring/ring-jetty-adapter "1.3.2"]])

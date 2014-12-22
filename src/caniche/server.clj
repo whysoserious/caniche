e2bfe294dbc081828134cd6e6bf0b9ff9b78612e
Jan Ziniewicz <janek.ziniewicz@gmai.com>
2014-12-22 22:29:41 +1100

    Ring server vol. 1

---
+++ src/caniche/server.clj
@@ -0,0 +1,21 @@
+(ns caniche.server
+  (require [compojure.core :refer :all]
+           [compojure.core :as route]
+           [ring.adapter.jetty :as ring]))
+
+(defroutes app
+  (GET "/" [] "<h1>Hello World!</h1>"))
+
+(defn -main []
+  (println "START")
+  (ring/run-jetty #'routes {:port 8080 :join? false}))
+
+
+
+
+
+
+
+
+
+

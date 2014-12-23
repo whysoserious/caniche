f4154daabf8a4a29102478a962a97c360e4d96d6
Jan Ziniewicz <janek.ziniewicz@gmai.com>
2014-12-24 00:27:28 +1100

    routes routes routes

--- src/caniche/server.clj
+++ src/caniche/server.clj
@@ -1,13 +1,29 @@
 (ns caniche.server
   (require [compojure.core :refer :all]
            [compojure.core :as route]
-           [ring.adapter.jetty :as ring]))
+           [ring.adapter.jetty :as ring]
+           [clojure.java.io :as io]))
 
 ;; TODO route not found
-(defroutes app
-  (GET "/" [] "<h1>Hello World!</h1>")
+(defroutes caniche-routes
+  (GET "/" [] "<h1>Hello World!</h1>\n")
+  (POST "/x"
+        params
+        {:status 200
+         :headers {"Content-Type" "text/html"}
+         :body (str params)})
   (POST "/upload"
         {{{tempfile :tempfile filename :filename} :file} :params :as params}
+;;        (io/copy tempfile (io/file "resources" "public" filename))
         {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body params}))
+         :headers {"Content-Type" "text/html"}
+         :body "OK\n"}))
+
+(defn log-request [app]
+  (fn [req]
+    (doseq [p req] (prn p))
+    (app req)))
+
+(def app
+  (-> caniche-routes
+      log-request))

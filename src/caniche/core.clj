d2ac638a12958624bf0cec9f3818def72d5e7844
Jan Ziniewicz <janek.ziniewicz@gmai.com>
2014-12-28 21:32:17 +1100

    Encrypt byte[] to String. Decrypt String to byte[]

--- src/caniche/core.clj
+++ src/caniche/core.clj
@@ -9,37 +9,35 @@
 
 ;; Encryption functions taken from http://stackoverflow.com/questions/10221257/is-there-an-aes-library-for-clojure
 
-(defn secret-key [size]
+(defn- secret-key [size]
   (crypto.random/base32 size))
 
-(defn bytes [s]
+(defn- bytes [s]
   (.getBytes s "UTF-8"))
 
-(defn base64 [b]
+(defn- base64 [b]
   (Base64/encodeBase64String b))
 
-(defn debase64 [s]
+(defn- debase64 [s]
   (Base64/decodeBase64 (bytes s)))
 
-(defn get-raw-key [seed]
+(defn- get-raw-key [seed]
   (let [keygen (KeyGenerator/getInstance "AES")
         sr (SecureRandom/getInstance "SHA1PRNG")]
     (.setSeed sr (bytes seed))
     (.init keygen 128 sr)
     (.. keygen generateKey getEncoded)))
 
-(defn get-cipher [mode seed]
+(defn- get-cipher [mode seed]
   (let [key-spec (SecretKeySpec. (get-raw-key seed) "AES")
         cipher (Cipher/getInstance "AES")]
     (.init cipher mode key-spec)
     cipher))
 
-(defn encrypt [text key]
-  (let [bytes (bytes text)
-        cipher (get-cipher Cipher/ENCRYPT_MODE key)]
+(defn encrypt [bytes key]
+  (let [cipher (get-cipher Cipher/ENCRYPT_MODE key)]
     (base64 (.doFinal cipher bytes))))
 
 (defn decrypt [text key]
   (let [cipher (get-cipher Cipher/DECRYPT_MODE key)]
-    (String. (.doFinal cipher (debase64 text)))))
-
+    (.doFinal cipher (debase64 text))))
